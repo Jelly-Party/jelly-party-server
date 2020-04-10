7d7c9505d08e2d10b40525e6995d35653602e5ef
seandlg <sean.eulenberg@rwth-aachen.de>
2020-04-10 17:03:34 +0200

    Delete api.js

--- api.js
+++
@@ -1,8 +0,0 @@
-// Server can receive the follwing instructions from client
-joinInstruction = { "type": "join", "clientName": "clientXYZ", "partyId": "someId" };
-forwardInstruction_videoUpdate = { "type": "forward", "partyId": "someId", "data": { "commandToForward": { "type": "videoUpdate", "data": { "variant": "playPause", "tick": 1000 } } } }
-
-// Client can receive the follwing instructions from server
-videoInstruction = { "type": "videoUpdate", "data": { "variant": "playPause", "tick": 1000 } }
-partyStateUpdateInstruction = { type: "partyStateUpdate", data: { partyState: { isActive: true, partyId: "someId", peers: [] } } };
-
